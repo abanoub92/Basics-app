bee04077dc5e5bf8c5dbce2013619c19d0e623f1
abanoub ghebreal <abanoub.eskander15@gmail.com>
2018-07-31 23:07:51 +0200

    Step two: adding list of toys names in array and display it in text view by looping the toys names and modify the scroll view for scrolling to get all undisplay list's data

--- app/src/main/java/com/abanoub/unit/basicsapp/MainActivity.java
+++ app/src/main/java/com/abanoub/unit/basicsapp/MainActivity.java
@@ -2,12 +2,76 @@ package com.abanoub.unit.basicsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** This textView represents a names for some kids toys */
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Create reference to an xml layout textView */
+        mTextView = findViewById(R.id.tv_toy_names);
+
+        /* Loop for display a list of toys names */
+        for (String toy : toys()){
+            mTextView.append(toy + "\n\n");
+        }
+    }
+
+    /**
+     * This simple method to save and hold the toys name
+     * @return toysNames for some kids toys names
+     */
+    private String[] toys(){
+        String[] toysNames = {
+                "Red Toy Wagon",
+                "Chemistry Set",
+                "Yo-Yo",
+                "Pop-Up Book",
+                "Generic Uncopyrighted Mouse",
+                "Finger Paint",
+                "Sock Monkey",
+                "Microscope Set",
+                "Beach Ball",
+                "BB Gun",
+                "Green Soldiers",
+                "Bubbles",
+                "Spring Toy",
+                "Fortune Telling Ball",
+                "Plastic Connecting Blocks",
+                "Water Balloon",
+                "Paint-by-Numbers Kit",
+                "Tuber Head",
+                "Cool Ball with Holes in It",
+                "Toy Truck",
+                "Flying Disc",
+                "Two-Handed Pogo Stick",
+                "Toy Hoop",
+                "Dysmorphia Doll",
+                "Toy Train",
+                "Fake Vomit",
+                "Toy Telephone",
+                "Barrel of Primates",
+                "Remote Control Car",
+                "Square Puzzle Cube",
+                "Football",
+                "Intergalactic Electronic Phasers",
+                "Baby Horse Dolls",
+                "Machines that turn into other Machines",
+                "Teddy Bears",
+                "Shaved Ice Maker",
+                "Glow Stick",
+                "Squirt Guns",
+                "Miniature Replica Animals Stuffed with Beads that you swore to your parents would be worth lots of money one day",
+                "Creepy Gremlin Doll",
+                "Neodymium-Magnet Toy"
+        };
+
+        return toysNames;
     }
 }
